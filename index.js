f80499de10acdf6c072def0d09cb923c5c313419
sapphira abayomi <saphieabayomi@gmail.com>
2018-11-26 10:50:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var newSentence = [];
+  for(let i = 0; i < musicians.length; i++){
+    newSentence.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return newSentence
+}
+
+function johnLennonFacts(facts){
+  var i = 0
+  var newString = []
+  while ( i < facts.length){
+    newString.push(`${facts[i]}!!!`)
+    i++
+  }
+
+  return newString
+}
